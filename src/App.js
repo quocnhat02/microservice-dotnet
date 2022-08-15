c722b78b531fa20fd68456cb6e7954b57627b11d
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-08-15 15:55:39 +0700

    useEffect

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <ExampleFunctional />
-      <ExampleClass />
+      {/* <ExampleClass /> */}
     </div>
   );
 }
--- src/components/ExampleFunctional.js
+++ src/components/ExampleFunctional.js
@@ -1,28 +1,52 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const ExampleFunctional = () => {
   const [count, setCount] = useState(0);
-  const [user, setUser] = useState({
-    name: "test",
-    age: 12,
-  });
+  const [action, setAction] = useState("");
+  const [scrollPosition, setScrollPosition] = useState(0);
 
-  const handleClick = () => {
-    setCount((preState) => {
-      return preState + 1;
-    });
-    setCount((preState) => {
-      return preState + 1;
-    });
+  useEffect(() => {
+    // componentDidMount and componentDidUpdate
+    document.title = `You clicked ${count} times`;
+    console.log("useEffect");
 
-    setUser({ name: "updated" });
+    return () => {
+      // clean-up function
+    };
+  }, [count]);
+
+  useEffect(() => {
+    fetch(`https://reqres.in/api/${action}`)
+      .then((res) => console.log({ res }))
+      .catch((err) => console.log(err));
+  }, [action]);
+
+  const handleScroll = () => {
+    setScrollPosition(window.scrollY);
   };
+
+  useEffect(() => {
+    // componentDidMount
+    document.addEventListener("scroll", handleScroll);
+
+    return () => {
+      // componentWillUnmount
+      document.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div>
+    <div style={{ heigh: "3000px" }}>
       <pre>Functional Component</pre>
       <p>You clicked {count} times</p>
-      <button onClick={handleClick}>Click me</button>
-      <p>{JSON.stringify(user)}</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+
+      <button onClick={() => setAction("users")}>Get User</button>
+      <button onClick={() => setAction("comments")}>Get Comment</button>
+
+      <p style={{ position: "fixed", bottom: "20px", left: "20px" }}>
+        {scrollPosition}
+      </p>
     </div>
   );
 };
