9d3a744e294fcf2eaed58cbfad9ba584fabb2f5c
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-08-16 19:24:54 +0700

    beginner learn hooks

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from "react"; // nạp thư viện react
-import ReactDOM from "react-dom"; // nạp thư viện react-dom
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-// Tạo component App
-function App() {
-  return (
-    <div>
-      <h1>Xin chào anh em F8!</h1>
-      <h2>Hello ae</h2>
-    </div>
-  );
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-// Render component App vào #root element
-ReactDOM.render(<App />, document.getElementById("root"));
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
