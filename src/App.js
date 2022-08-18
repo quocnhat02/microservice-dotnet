3f5cb9a8274e77f7bfd52df44349ceb89b501b9e
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-08-18 22:32:45 +0700

    useState two-way binding

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React, { useState } from "react";
 
-const RAM = ["8 GB", "16 GB", "32 GB"];
-
 function App() {
-  const [ram, setRam] = useState(RAM[0]);
-
-  const handleRAM = () => {
-    setRam(() => {
-      let index = Math.floor(Math.random() * RAM.length);
-      console.log(RAM[index]);
-      return RAM[index];
-    });
+  const [name, setName] = useState("");
+  console.log(name);
+  const handleInput = (e) => {
+    setName(e.target.value);
   };
-
   return (
     <div className="App" style={{ padding: 30 }}>
-      <h1>{ram}</h1>
-      <button onClick={handleRAM}>Get</button>
+      <input type="text" value={name} onChange={handleInput} />
+      <button onClick={() => setName("Nguyen Van bbb")}>Change</button>
     </div>
   );
 }
