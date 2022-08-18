66609ea0eef097d970b6a0d16af313afd98b00b7
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-08-18 22:36:38 +0700

    useState two-way binding form

--- src/App.js
+++ src/App.js
@@ -2,14 +2,33 @@ import React, { useState } from "react";
 
 function App() {
   const [name, setName] = useState("");
-  console.log(name);
-  const handleInput = (e) => {
-    setName(e.target.value);
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = () => {
+    console.log({
+      name,
+      email,
+    });
   };
+
+  console.log(name);
   return (
     <div className="App" style={{ padding: 30 }}>
-      <input type="text" value={name} onChange={handleInput} />
-      <button onClick={() => setName("Nguyen Van bbb")}>Change</button>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      />
+      <input
+        type="text"
+        value={email}
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
+      <button onClick={handleSubmit}>Register</button>
     </div>
   );
 }
