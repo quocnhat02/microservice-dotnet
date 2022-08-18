c903c7f8765ad50ae34fc94bce5e90b181c4af35
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-08-18 23:11:39 +0700

    useState two-way binding checkbox array

--- src/App.js
+++ src/App.js
@@ -16,9 +16,24 @@ const courses = [
 ];
 
 function App() {
-  const [checked, setChecked] = useState(1);
+  const [checked, setChecked] = useState([]);
+
+  const handleCheck = (id) => {
+    setChecked((prev) => {
+      // const isChecked = checked.includes(id);
+      // if (isChecked) {
+      //   return checked.filter((item) => item !== id);
+      // } else {
+      //   return [...prev, id];
+      // }
+      return checked.includes(id)
+        ? checked.filter((item) => item !== id)
+        : [...prev, id];
+    });
+  };
+
   const handleSubmit = () => {
-    console.log({ id: checked });
+    console.log({ ids: checked });
   };
 
   return (
@@ -26,10 +41,10 @@ function App() {
       {courses.map((course) => (
         <div key={course.id}>
           <input
-            type="radio"
+            type="checkbox"
             id={course.id}
-            checked={checked === course.id}
-            onChange={() => setChecked(course.id)}
+            checked={checked.includes(course.id)}
+            onChange={() => handleCheck(course.id)}
           />
           <label htmlFor={course.id}>{course.name}</label>
         </div>
