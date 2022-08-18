08875fd91267cde20b592f84c8436eb5bb83ad57
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-08-18 23:25:34 +0700

    useState todo-list

--- src/App.js
+++ src/App.js
@@ -1,56 +1,24 @@
 import React, { useState } from "react";
 
-const courses = [
-  {
-    id: 1,
-    name: "HTML, CSS",
-  },
-  {
-    id: 2,
-    name: "Javascript",
-  },
-  {
-    id: 3,
-    name: "ReactJS",
-  },
-];
-
 function App() {
-  const [checked, setChecked] = useState([]);
-
-  const handleCheck = (id) => {
-    setChecked((prev) => {
-      // const isChecked = checked.includes(id);
-      // if (isChecked) {
-      //   return checked.filter((item) => item !== id);
-      // } else {
-      //   return [...prev, id];
-      // }
-      return checked.includes(id)
-        ? checked.filter((item) => item !== id)
-        : [...prev, id];
-    });
-  };
+  const [job, setJob] = useState("");
+  const [jobs, setJobs] = useState([]);
 
   const handleSubmit = () => {
-    console.log({ ids: checked });
+    setJobs((prev) => [...prev, job]);
+    setJob("");
   };
 
   return (
     <div className="App" style={{ padding: 30 }}>
-      {courses.map((course) => (
-        <div key={course.id}>
-          <input
-            type="checkbox"
-            id={course.id}
-            checked={checked.includes(course.id)}
-            onChange={() => handleCheck(course.id)}
-          />
-          <label htmlFor={course.id}>{course.name}</label>
-        </div>
-      ))}
+      <input value={job} type="text" onChange={(e) => setJob(e.target.value)} />
+      <button onClick={handleSubmit}>Add</button>
 
-      <button onClick={handleSubmit}>Register</button>
+      <ul>
+        {jobs.map((job, index) => (
+          <li key={index}>{job}</li>
+        ))}
+      </ul>
     </div>
   );
 }
