a32ab0d018f24d8eb66167750b088f519755b166
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-08-22 10:46:07 +0700

    use compare true or false

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import React, { useState } from "react";
 
 const App = () => {
   const name = "John";
+  const isNameShowing = false;
 
   return (
     <div className="App" style={{ padding: 30 }}>
-      <h1>Hello, {name}</h1>
+      <h1>Hello, {isNameShowing ? name : "someone"}</h1>
     </div>
   );
 };
