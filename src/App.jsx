2221b0f400d2db8106386a2dc2ce742de56f5684
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-08-22 11:07:33 +0700

    create Person

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
 
-const App = () => {
-  const name = "John";
-  const isNameShowing = false;
+const Person = ({ firstName, lastName, age }) => {
+  return (
+    <>
+      <h1>FistName: {firstName}</h1>
+      <h1>LastName: {lastName}</h1>
+      <h1>Age: {age}</h1>
+    </>
+  );
+};
 
+const App = () => {
   return (
     <div className="App" style={{ padding: 30 }}>
-      <h1>Hello, {!isNameShowing && name}</h1>
+      <Person firstName="Nhat" lastName="Nguyen" age="20" />
+      <Person firstName="Thuan" lastName="Trieu" age="21" />
+      <Person firstName="Dai" lastName="Trieu" age="22" />
     </div>
   );
 };
