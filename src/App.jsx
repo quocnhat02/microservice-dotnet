b64a9b3721f5b781c4d2b0374557cd9b2d57ce82
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-08-22 10:45:01 +0700

    hello

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const name = "John";
+
+  return (
+    <div className="App" style={{ padding: 30 }}>
+      <h1>Hello, {name}</h1>
+    </div>
+  );
+};
+
+export default App;
