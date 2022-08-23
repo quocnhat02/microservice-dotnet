587f8c747dba621a5639108dd6777568f62f666e
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-08-23 20:34:44 +0700

    create navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,11 @@
-import { useEffect, useState } from "react";
+import React from "react";
+import Navbar from "./components/Navbar";
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() => {
-    alert("You changed the counter to" + counter);
-  }, [counter]);
-
   return (
-    <div className="App" style={{ padding: 30 }}>
-      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>
-        -
-      </button>
-      <h1>{counter}</h1>
-      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>
-        +
-      </button>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 };
 
