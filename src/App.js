ce6618a9ac03211169e635b16e785b4a2ba1aab2
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-08-27 22:26:19 +0700

    building react app 3.2.5

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React, { Fragment } from "react";
+import React, { Component } from "react";
 import CardList from "./CardList";
 import { robots } from "./robots";
 import SearchBox from "./SearchBox";
 
-const App = () => {
-  return (
-    <div className="tc">
-      <h1>Robot Friends</h1>
-      <SearchBox />
-      <CardList robots={robots} />
-    </div>
-  );
-};
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots,
+      searchfield: "",
+    };
+  }
+
+  onSearchChange = (event) => {
+    const filteredRobots = this.state.robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+    console.log(filteredRobots);
+  };
+
+  render() {
+    return (
+      <div className="tc">
+        <h1>Robot Friends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={this.state.robots} />
+      </div>
+    );
+  }
+}
 
 export default App;
