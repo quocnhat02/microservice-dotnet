81c321c6a8f8fead70d84b460151ccb56d368cfb
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-08-27 21:40:04 +0700

    building react app 3.2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Fragment } from "react";
 import CardList from "./CardList";
 import { robots } from "./robots";
+import SearchBox from "./SearchBox";
 
 const App = () => {
   return (
     <Fragment>
+      <h1>Robot Friends</h1>
+      <SearchBox />
       <CardList robots={robots} />
     </Fragment>
   );
