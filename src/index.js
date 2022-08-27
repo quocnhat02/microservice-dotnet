ff49e5768a53d23c0990fe0bfdc075e76721b5b2
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-08-27 20:40:45 +0700

    building react app 1.1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Hello from "./Hello";
+import Card from "./Card";
 import reportWebVitals from "./reportWebVitals";
+import "tachyons";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Hello />
+    <Card />
+    <Card />
+    <Card />
   </React.StrictMode>
 );
 
