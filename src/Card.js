4e614bd800a031cf3555677f75baf4f69313dec4
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-08-27 20:49:49 +0700

    building react app 1.2

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@
 
 const Card = () => {
   return (
-    <div className="bg-light-green dib br3 pa3 ma2 grow">
+    <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
       <img src="https://robohash.org/test?200x200" alt="robots" />
       <div>
         <h2>Jane Doe</h2>
