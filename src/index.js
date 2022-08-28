be7370a495e23f8490b83e6e1c481ffce4ee070c
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-08-28 22:52:29 +0700

    redux store and provider 1

--- src/index.js
+++ src/index.js
@@ -13,8 +13,8 @@ const store = createStore(searchRobots);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider>
-      <App store={store} />
+    <Provider store={store}>
+      <App />
     </Provider>
   </React.StrictMode>
 );
