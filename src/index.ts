ecdb9b9e99e461d54e34f5af4b6646b9cb9dde64
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-09-16 16:09:10 +0700

    type unknow

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-let firstName: any = "Light";
-
-firstName = 123;
-
-firstName = [1, 2, 3];
-
-function multiByTwo(number) {
-  return number * 2;
+function multiByTwo(number: unknown) {
+  if (typeof number === "number") {
+    return number * 2;
+  }
+  return "Please provide a valid number";
 }
+
+console.log(multiByTwo(5));
+console.log(multiByTwo("5"));
