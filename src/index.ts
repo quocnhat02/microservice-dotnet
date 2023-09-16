8a6dff72fad57e606407d1d4484ec952dbaa0ff1
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-09-16 23:45:45 +0700

    polymophic

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,28 @@
-type Reservation = {
-  departureDate: Date;
-  returnDate?: Date;
-  departingFrom: string;
-  destination: string;
-};
+function filter(array: any[], predicate: Function) {
+  let result: any[] = [];
 
-type Reserve = {
-  (
-    departureDate: Date,
-    returnDate: Date,
-    departingFrom: string,
-    destination: string,
-  ): Reservation | never;
-  (
-    departureDate: Date,
-    departingFrom: string,
-    destination: string,
-  ): Reservation | never;
-};
-
-const reserve: Reserve = (
-  departureDate: Date,
-  returnDateOrDepartingFrom: Date | string,
-  departingFromOrDestination: string,
-  destination?: string,
-) => {
-  if (returnDateOrDepartingFrom instanceof Date && destination) {
-    return {
-      departureDate,
-      returnDate: returnDateOrDepartingFrom,
-      departingFrom: departingFromOrDestination,
-      destination: destination,
-    };
-  } else if (typeof returnDateOrDepartingFrom === "string") {
-    return {
-      departureDate,
-      departingFrom: returnDateOrDepartingFrom,
-      destination: departingFromOrDestination,
-    };
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    if (predicate(element)) {
+      result.push(element);
+    }
   }
-  throw new Error("Please provide valid details to reserve a ticket");
-};
+
+  return result;
+}
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function greaterThanSeven(item: number) {
+  return item > 7;
+}
+
+console.log(filter(numbers, greaterThanSeven));
+
+let animals = ["cat", "dog", "lion"];
+
+function filterCats(item: string) {
+  return item === "cat";
+}
+
+console.log(filter(animals, filterCats));
---
+++ src/types/overloading.type.ts
@@ -0,0 +1,43 @@
+type Reservation = {
+  departureDate: Date;
+  returnDate?: Date;
+  departingFrom: string;
+  destination: string;
+};
+
+type Reserve = {
+  (
+    departureDate: Date,
+    returnDate: Date,
+    departingFrom: string,
+    destination: string,
+  ): Reservation | never;
+  (
+    departureDate: Date,
+    departingFrom: string,
+    destination: string,
+  ): Reservation | never;
+};
+
+const reserve: Reserve = (
+  departureDate: Date,
+  returnDateOrDepartingFrom: Date | string,
+  departingFromOrDestination: string,
+  destination?: string,
+) => {
+  if (returnDateOrDepartingFrom instanceof Date && destination) {
+    return {
+      departureDate,
+      returnDate: returnDateOrDepartingFrom,
+      departingFrom: departingFromOrDestination,
+      destination: destination,
+    };
+  } else if (typeof returnDateOrDepartingFrom === "string") {
+    return {
+      departureDate,
+      departingFrom: returnDateOrDepartingFrom,
+      destination: departingFromOrDestination,
+    };
+  }
+  throw new Error("Please provide valid details to reserve a ticket");
+};
