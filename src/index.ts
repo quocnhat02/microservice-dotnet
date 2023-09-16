f55a589883bceafe9d97bb1596cd357a8cf74861
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-09-16 20:19:18 +0700

    function

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,7 @@
-import { Type } from "./roles";
+function intro(name: string, age: number): string {
+  return `My name is ${name} and age is ${age}`;
+}
 
-// type TypeOfLibrary = 'national' | 'academic'|'public'
-
-type Book = {
-  title: string;
-  pages: number;
-  isbn: string;
-};
-
-type Member = {
-  name: string;
-  phone: string;
-  [key: string]: string;
-};
-
-type Library = {
-  name: string;
-  address: string;
-  numberOfBooks: number;
-  type: Type;
-  books: Book[];
-  genres: string[];
-  members: Member[];
-};
-
-const library: Library = {
-  name: "New York Library",
-  address: "New York",
-  numberOfBooks: 1234,
-  type: Type.national,
-  books: [
-    {
-      title: "Nodejs",
-      pages: 250,
-      isbn: "988-66-443",
-    },
-  ],
-  genres: ["backend"],
-  members: [
-    {
-      name: "Nhat",
-      phone: "09999999",
-      email: "nhat@gmail.com",
-    },
-  ],
+const intro2 = (name: string, age: number): string => {
+  return `My name is ${name} and age is ${age}`;
 };
